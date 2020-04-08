47cae0471aba03915567d4c2f0826ba9e3ffab37
Florian Kapfenberger <florian@kapfenberger.me>
2020-04-08 22:49:55 +0200

    update build script

--- build.sh
+++ build.sh
@@ -1,1 +1,11 @@
+#!/bin/bash
 cd responsive-web-design/build-a-survey-form && npm run build && cd -
+
+rm -rf public
+
+mkdir public
+mkdir -p public/responsive-web-design/build-a-survey-form
+echo "created directoy: public/responsive-web-design/build-a-survey-form"
+
+cp -R responsive-web-design/build-a-survey-form/dist/* public/responsive-web-design/build-a-survey-form
+echo "copied files to public/responsive-web-design/build-a-survey-form"
