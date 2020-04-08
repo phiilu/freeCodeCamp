60dbe875d681c319c57636fda3791334f58d84c4
Florian Kapfenberger <florian@kapfenberger.me>
2020-04-08 22:59:46 +0200

    update build.sh and add root index.html

--- build.sh
+++ build.sh
@@ -9,3 +9,10 @@ echo "created directoy: public/responsive-web-design/build-a-survey-form"
 
 cp -R responsive-web-design/build-a-survey-form/dist/* public/responsive-web-design/build-a-survey-form
 echo "copied files to public/responsive-web-design/build-a-survey-form"
+
+mv public/responsive-web-design/build-a-survey-form/*.css public
+mv public/responsive-web-design/build-a-survey-form/*.js public
+mv public/responsive-web-design/build-a-survey-form/*.map public
+
+cp src/index.html public
+echo "copied root index.html"
